73d97dae941e1bb4396fd02dad431ea05c75c602
Dmitriy <dimaprostokosmos@gmail.com>
2022-09-07 14:21:44 +0300

    added news form

--- app.js
+++ app.js
@@ -55,15 +55,16 @@ function customHttp() {
 }
 // Init http module
 const http = customHttp();
+let lastSearch = "";
 
 const newsServise = (function () {
   const apiKey = "921c67a7cace41b9955b82352b983dce";
   const apiUrl = "https://newsapi.org/v2";
 
   return {
-    topHeadLines(country = "ua", cb) {
+    topHeadLines(country = "ua", category = "technology", cb) {
       http.get(
-        `${apiUrl}/top-headlines?country=${country}&category=technology&apiKey=${apiKey}`,
+        `${apiUrl}/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}`,
         cb
       );
     },
@@ -72,6 +73,17 @@ const newsServise = (function () {
     },
   };
 })();
+//elements
+const form = document.forms["newsControls"];
+const countrySelect = form.elements["country"];
+const categorySelect = form.elements["category"];
+const searchInput = form.elements["search"];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  loadNews();
+});
+
 //  init selects
 document.addEventListener("DOMContentLoaded", function () {
   M.AutoInit();
@@ -80,36 +92,70 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // ------- load newsFunction--------
 function loadNews() {
-  newsServise.topHeadLines("ua", onGetResponse);
+  showLoader();
+  const category = categorySelect.value;
+  const country = countrySelect.value;
+  lastSearch = searchInput.value;
+
+  if (!lastSearch) {
+    newsServise.topHeadLines(country, category, onGetResponse);
+  } else {
+    newsServise.everything(lastSearch, onGetResponse);
+  }
+  console.log(lastSearch);
 }
 
 // function on get response from server----
 function onGetResponse(err, res) {
-  console.log(res);
+  removePreloader();
+
+  if (err) {
+    showAlert(err, "error-msg");
+    return;
+  }
+  if (!res.articles.length) {
+    renderEmptyMessage();
+    return;
+  }
   renderNews(res.articles);
 }
 
 // function render news
 function renderNews(news) {
   const newsContainer = document.querySelector(".news-container .row");
+  if (newsContainer.children.length) {
+    clearContainer(newsContainer);
+  }
+
   let fragment = "";
   news.forEach((newsItem) => {
     const el = newsTemplate(newsItem);
     fragment += el;
   });
-  console.log(fragment);
   newsContainer.insertAdjacentHTML("afterBegin", fragment);
+}
+
+// function clear container
+function clearContainer(container) {
+  //container.innerHTML = '';
+  let child = container.lastElementChild;
+  while (child) {
+    container.removeChild(child);
+    child = container.lastElementChild;
+  }
 }
 
 //----- News item template function
 function newsTemplate(news) {
-  console.log(news);
+  // console.log(news);
+  const defaultSrc =
+    "https://img.freepik.com/premium-photo/astronaut-outer-open-space-planet-earth-stars-provide-background-erforming-space-planet-earth-sunrise-sunset-our-home-iss-elements-this-image-furnished-by-nasa_150455-16829.jpg?w=2000";
   const { urlToImage, title, description, url } = news;
   return `
     <div class="col s12">
       <div class="card">
         <div class="card-image">
-          <img src="${urlToImage}">
+          <img src="${urlToImage || defaultSrc}">
           <span class="card-title">${title || ""}</span>
         </div>
         <div class="card-content">
@@ -122,3 +168,42 @@ function newsTemplate(news) {
     </div>
   `;
 }
+
+function showAlert(msg, type = "success") {
+  M.toast({ html: msg, classes: type });
+}
+
+//show loader function
+function showLoader() {
+  document.body.insertAdjacentHTML(
+    "afterbegin",
+    `
+    <div class='progress'>
+      <div class='indeterminate'></div>
+    </div>
+    `
+  );
+}
+
+//Remove loader function
+function removePreloader() {
+  const loader = document.querySelector(".progress");
+  if (loader) {
+    loader.remove();
+  }
+}
+
+// function emply message
+
+function renderEmptyMessage() {
+  const newsContainer = document.querySelector(".news-container .row");
+  if (newsContainer.children.length) {
+    clearContainer(newsContainer);
+  }
+  const emptyTextMsg = `По запросу "${lastSearch}" ничего не найдено`;
+  newsContainer.insertAdjacentHTML(
+    "afterbegin",
+    `<div class='empty-message'>${emptyTextMsg}</div>`
+  );
+}
+
